c2c0095a7d1e0925cc5c58be5522e2dcf3867720
王时青 <sophomore@duck.com>
2025-02-04 22:28:36 +0800

    just clean up code

--- main.cpp
+++ main.cpp
@@ -5,27 +5,22 @@ static void print_hello(GtkWidget *widget, gpointer data) {
 }
 
 static void activate(GtkApplication *app, gpointer user_data) {
-  GtkWidget *window;
-  GtkWidget *button;
+  auto window = gtk_application_window_new(app);
+  auto gtk_window = GTK_WINDOW(window);
+  gtk_window_set_title(gtk_window, "Hello");
+  gtk_window_set_default_size(gtk_window, 200, 200);
 
-  window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "Hello");
-  gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
-
-  button = gtk_button_new_with_label("Hello World");
+  auto button = gtk_button_new_with_label("Hello World");
   g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-  gtk_window_set_child(GTK_WINDOW(window), button);
-
-  gtk_window_present(GTK_WINDOW(window));
+  gtk_window_set_child(gtk_window, button);
+  
+  gtk_window_present(gtk_window);
 }
 
 int main(int argc, char **argv) {
-  GtkApplication *app;
-  int status;
-
-  app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  auto app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-  status = g_application_run(G_APPLICATION(app), argc, argv);
+  const auto status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
 
   return status;
