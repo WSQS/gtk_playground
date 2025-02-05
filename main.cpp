f6f1d9183fc138c82cc92aaca1541126015ba433
WSQSylock <wsqsylock@gmail.com>
2025-02-05 21:29:27 +0800

    clean up code

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 static void print_hello(GtkWidget *widget, gpointer data) {
   g_print("Hello World\n");
 }
-static auto add_number(GtkGrid *grid, int number) {
-  auto button = gtk_button_new_with_label(std::to_string(number).c_str());
+inline auto add_number(GtkGrid *grid, int number) {
+  auto button = gtk_button_new_with_label(std::to_string(number).data());
   gtk_grid_attach(grid, button, (number - 1) % 3, (number - 1) / 3, 1, 1);
-  g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+  g_signal_connect(button, "clicked", G_CALLBACK(print_hello), nullptr);
 }
 static void activate(GtkApplication *app, gpointer user_data) {
   auto window = gtk_application_window_new(app);
@@ -23,7 +23,7 @@ static void activate(GtkApplication *app, gpointer user_data) {
   gtk_window_set_child(GTK_WINDOW(window), box);
 
   auto button = gtk_button_new_with_label("Hello World");
-  g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+  g_signal_connect(button, "clicked", G_CALLBACK(print_hello), nullptr);
   g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_window_close),
                            gtk_window);
   gtk_box_append(GTK_BOX(box), grid);
@@ -34,7 +34,7 @@ static void activate(GtkApplication *app, gpointer user_data) {
 int main(int argc, char **argv) {
   auto app =
       gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), nullptr);
   const auto status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
 
